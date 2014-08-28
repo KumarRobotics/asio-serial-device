6aec4e809b545bd1f6df7efe2ffb3e21850060b1
Kartik Mohta <kartikmohta@gmail.com>
2014-08-28 16:59:14 -0400

    Renaming to asio_serial_device, adding travis.yml for this branch

---
+++ README.md
@@ -0,0 +1,4 @@
+# asio_serial_device
+
+[![Build
+Status](https://travis-ci.org/KumarRobotics/asio_serial_device.svg?branch=rosbuild)](https://travis-ci.org/KumarRobotics/asio_serial_device)
--- src/ASIOSerialDevice.cc
+++ src/ASIOSerialDevice.cc
@@ -1,8 +1,8 @@
 /*
-  This file is part of asio-serial-device, a class wrapper to
+  This file is part of asio_serial_device, a class wrapper to
   use the boost::asio serial functionality.
 
-  asio-serial-device is free software: you can redistribute it and/or modify
+  asio_serial_device is free software: you can redistribute it and/or modify
   it under the terms of the GNU General Public License as published by
   the Free Software Foundation, either version 3 of the License, or
   (at your option) any later version.
